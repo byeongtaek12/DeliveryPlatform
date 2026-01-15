32f23f7958c3837367061ba1352f2c1e8892e996
obt <byeongtaek12@naver.com>
2026-01-15 17:17:42 +0900

    first commit

---
+++ src/test/java/com/example/deliveryplatform/DeliveryPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.deliveryplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
