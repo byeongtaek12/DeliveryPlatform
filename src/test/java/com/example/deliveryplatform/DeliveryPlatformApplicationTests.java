c08c8035eba9b6e25323b2d05e7229f07b54042d
obt <byeongtaek12@naver.com>
2026-01-26 20:22:17 +0900

    test: 회원가입 통합 테스트 추가(#1)
    ``

--- src/test/java/com/example/deliveryplatform/DeliveryPlatformApplicationTests.java
+++ src/test/java/com/example/deliveryplatform/DeliveryPlatformApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DeliveryPlatformApplicationTests {
+class DeliveryPlatformApplicationTests extends MySQLContainerBaseTest{
 
 	@Test
 	void contextLoads() {
