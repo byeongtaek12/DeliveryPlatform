d6d2cb38a01c646ca9238c141360ce42e7c7b97c
obt <byeongtaek12@naver.com>
2026-02-04 20:25:09 +0900

    feat: CustomUserDetails 구현 & CustomUserDetailsService 구현(#3)
    - 로그인 요청일 때는 Security의 흐름인 manager -> provider를 태워 loadUserByUsername 메서드를 통해 내부적으로 패스워드까지 검증하고 accessToken을 내려줌
    
    - 다른 요청일 때는 위의 흐름을 태우지 않고 loadUserById 메서드를 통해 accessToken의 claim에서 id를 빼서 id를 통해 다시 로딩하고 인증 객체를 만들어securityContextHolder에 채워서 사용하는 방식
    
    - 그냥 UserDetailsService를 구현하게 되면 loadUserByUsername과 loadUserById를 구현한 구현체에 의존해야하는데 CustomUserDetailsService를 통해 UserDetailsService를 상속받아 인터페이스로 확장시키면 CustomUserDetailsService 인터페이스에 의존하고 구현체는 몰라도 되기 때문에 유지보수가 쉬워짐
    
    - CustomUserDetails가 CredentialsContainer를 구현하면 로그인 흐름에서 로그인 인증이 끝나고 반환된 Authentication에 대해 credentials를 내부적으로 지우게 되는데 지워질 때 UserDetails에 있는 credentials도 같이 지워지게 됨

---
+++ src/main/java/com/example/deliveryplatform/common/security/CustomUserDetailsService.java
@@ -0,0 +1,8 @@
+package com.example.deliveryplatform.common.security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public interface CustomUserDetailsService extends UserDetailsService {
+	UserDetails loadUserById(Long id);
+}
---
+++ src/main/java/com/example/deliveryplatform/common/security/CustomUserDetailsServiceImpl.java
@@ -0,0 +1,35 @@
+package com.example.deliveryplatform.common.security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.deliveryplatform.common.exception.BaseException;
+import com.example.deliveryplatform.common.exception.ErrorCode;
+import com.example.deliveryplatform.domain.user.entity.User;
+import com.example.deliveryplatform.domain.user.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+@Transactional(readOnly = true)
+public class CustomUserDetailsServiceImpl implements CustomUserDetailsService{
+	private final UserRepository userRepository;
+
+	@Override
+	public UserDetails loadUserById(Long id) {
+		User foundUser = userRepository.findById(id).orElseThrow(() -> new BaseException(ErrorCode.USER_NOT_FOUND));
+
+		return CustomUserDetails.fromJwt(foundUser);
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		User foundUser = userRepository.findByEmail(email)
+			.orElseThrow(() -> new UsernameNotFoundException("해당 email을 가진 유저가 없습니다."));
+
+		return CustomUserDetails.fromLogin(foundUser);
+	}
+}
