89cc28a7260703701e8320604d0ac7ce0b27bd18
obt <byeongtaek12@naver.com>
2026-02-05 15:28:42 +0900

    refactor: SecurityConfig) JwtFilter 등록 & 매개변수 주입 방식 사용(#3)
    - jwtFilter를 UsernamePasswordAuthenticationFilter 앞에 등록
    
    - SecurityConfig 자체에 자주 사용되지 않는 필드를 들고 있기 보단 매개변수를 통해 주입받는 방식이 맞는 것 같아서 적용

--- src/main/java/com/example/deliveryplatform/common/security/SecurityConfig.java
+++ src/main/java/com/example/deliveryplatform/common/security/SecurityConfig.java
@@ -12,11 +12,14 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.servlet.util.matcher.PathPatternRequestMatcher;
 import org.springframework.security.web.util.matcher.OrRequestMatcher;
 import org.springframework.security.web.util.matcher.RequestMatcher;
 
 import com.example.deliveryplatform.common.jwt.JwtFilter;
+import com.example.deliveryplatform.common.jwt.JwtTokenExtractor;
+import com.example.deliveryplatform.common.jwt.JwtTokenProvider;
 
 import jakarta.servlet.Filter;
 
@@ -35,14 +38,15 @@ public class SecurityConfig {
 	}
 
 	@Bean
-	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+	public SecurityFilterChain filterChain(HttpSecurity http, Filter jwtFilter, RequestMatcher publicEndpoints) throws Exception {
 		http.csrf(AbstractHttpConfigurer::disable)
 			.sessionManagement(sm ->
 				sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 			.authorizeHttpRequests(auth -> auth
-				.requestMatchers(publicEndpoints())
+				.requestMatchers(publicEndpoints)
 				.permitAll()
-				.anyRequest().authenticated());
+				.anyRequest().authenticated())
+			.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 		return http.build();
 	}
 
@@ -52,8 +56,9 @@ public class SecurityConfig {
 	}
 
 	@Bean
-	public Filter jwtFilter(RequestMatcher publicEndPoints) {
-		return new JwtFilter(publicEndPoints);
+	public JwtFilter jwtFilter(RequestMatcher publicEndPoints, JwtTokenExtractor jwtTokenExtractor,
+		JwtTokenProvider jwtTokenProvider) {
+		return new JwtFilter(publicEndPoints, jwtTokenExtractor, jwtTokenProvider);
 	}
 
 	@Bean
