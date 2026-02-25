08fcaff0af4ec909ba4339c6f14c053604f0dffd
obt <byeongtaek12@naver.com>
2026-02-25 23:44:01 +0900

    feat: LoginResponse) 정적 팩터리 메서드 추가(#5)
    - 외부에서 생성할 수 없게 생성자는 private으로 두고 정적 팩터리 메서드를 열어 유지보수하기 쉽고 의미 명확히 함

--- src/main/java/com/example/deliveryplatform/domain/auth/dto/LoginResponse.java
+++ src/main/java/com/example/deliveryplatform/domain/auth/dto/LoginResponse.java
@@ -6,4 +6,15 @@ import lombok.Getter;
 public class LoginResponse {
 	private Long id;
 	private String accessToken;
+
+	private LoginResponse(Long id, String accessToken) {
+		this.id = id;
+		this.accessToken = accessToken;
+	}
+
+	public static LoginResponse of(Long id, String accessToken) {
+		return new LoginResponse(id, accessToken);
+	}
+
+
 }
