673e9fff15aba3104b93c709edc592b31fc9d00a
obt <byeongtaek12@naver.com>
2026-02-26 23:07:18 +0900

    refactor: AuthController) 필요없는 import문 제거
    ``

--- src/main/java/com/example/deliveryplatform/domain/auth/controller/AuthController.java
+++ src/main/java/com/example/deliveryplatform/domain/auth/controller/AuthController.java
@@ -1,7 +1,6 @@
 package com.example.deliveryplatform.domain.auth.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.parameters.P;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
