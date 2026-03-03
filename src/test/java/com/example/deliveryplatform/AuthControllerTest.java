f8185e490511b89675e09b741bf0f89a0246722d
obt <byeongtaek12@naver.com>
2026-03-03 20:53:50 +0900

    test: AuthControllerTest) 로그인 관련 테스트 추가
    - 로그인 성공 테스트 & 로그인 실패 테스트 2개(인증실패 시 예외작동, validation 작동)

--- src/test/java/com/example/deliveryplatform/AuthControllerTest.java
+++ src/test/java/com/example/deliveryplatform/AuthControllerTest.java
@@ -8,6 +8,8 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -15,6 +17,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import com.example.deliveryplatform.common.exception.code.ErrorCode;
 import com.example.deliveryplatform.common.exception.customException.BaseException;
 import com.example.deliveryplatform.domain.auth.controller.AuthController;
+import com.example.deliveryplatform.domain.auth.dto.LoginRequest;
+import com.example.deliveryplatform.domain.auth.dto.LoginResponse;
 import com.example.deliveryplatform.domain.auth.dto.SignupRequest;
 import com.example.deliveryplatform.domain.auth.dto.SignupResponse;
 import com.example.deliveryplatform.domain.auth.service.AuthService;
@@ -90,6 +94,54 @@ public class AuthControllerTest {
 			.andExpect(status().isConflict())
 			.andExpect(jsonPath("$.errorCode").value("USER_001"))
 			.andExpect(jsonPath("$.message").value("이미 존재하는 이메일입니다."));
+	}
+
+	@Test
+	void loginSuccess() throws Exception {
+		LoginRequest req = new LoginRequest(
+			"byeongtaek12@gmail.com",
+			"1234"
+		);
+
+		LoginResponse res = LoginResponse.of(1L, "testToken");
+
+		given(authService.login(any(LoginRequest.class))).willReturn(res);
+
+		mockMvc.perform(post("/api/auth/login")
+			.contentType(MediaType.APPLICATION_JSON)
+			.content(objectMapper.writeValueAsString(req)))
+			.andExpect(status().isOk())
+			.andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+			.andExpect(jsonPath("$.id").value(1L))
+			.andExpect(jsonPath("$.accessToken").value("testToken"));
+	}
+
+	@Test
+	void loginFailWrongPassword() throws Exception {
+		LoginRequest req = new LoginRequest(
+			"byeongtaek12@gmail.com",
+			"1"
+		);
+
+		given(authService.login(any(LoginRequest.class))).willThrow(new BaseException(ErrorCode.INVALID_CREDENTIALS));
+
+		mockMvc.perform(post("/api/auth/login")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(req)))
+			.andExpect(status().isUnauthorized());
+	}
+
+	@Test
+	void loginFailValidation() throws Exception {
+		LoginRequest req = new LoginRequest(
+			"byeongtaek12@gmail.com",
+			""
+		);
+
+		mockMvc.perform(post("/api/auth/login")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(req)))
+			.andExpect(status().isBadRequest());
 	}
 
 
