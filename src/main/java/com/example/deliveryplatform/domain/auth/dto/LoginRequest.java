e22a8ff49d65c20e3c2802211c3f759c1d98a2f5
obt <byeongtaek12@naver.com>
2026-03-06 16:44:47 +0900

    refactor: RequestDto) validation 메시지 추가
    ``

--- src/main/java/com/example/deliveryplatform/domain/auth/dto/LoginRequest.java
+++ src/main/java/com/example/deliveryplatform/domain/auth/dto/LoginRequest.java
@@ -8,9 +8,9 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public class LoginRequest {
-	@Email
-	@NotBlank
+	@Email(message = "이메일 형식이 아닙니다.")
+	@NotBlank(message = "빈 값이 이메일이 될 순 없습니다.")
 	private String email;
-	@NotBlank
+	@NotBlank(message = "빈 값이 패스워드가 될 순 없습니다.")
 	private String password;
 }
--- src/main/java/com/example/deliveryplatform/domain/auth/dto/SignupRequest.java
+++ src/main/java/com/example/deliveryplatform/domain/auth/dto/SignupRequest.java
@@ -8,15 +8,15 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public class SignupRequest {
-	@Email
-	@NotBlank
+	@Email(message = "이메일 형식이 아닙니다.")
+	@NotBlank(message = "빈 값이 이메일이 될 순 없습니다.")
 	private String email;
-	@NotBlank
+	@NotBlank(message = "빈 값이 패스워드가 될 순 없습니다.")
 	private String password;
-	@NotBlank
+	@NotBlank(message = "빈 값이 닉네임이 될 순 없습니다.")
 	private String nickname;
-	@NotBlank
+	@NotBlank(message = "빈 값이 번호가 될 순 없습니다.")
 	private String phoneNumber;
-	@NotBlank
+	@NotBlank(message = "빈 값이 역할이 될 순 없습니다.")
 	private String role;
 }
